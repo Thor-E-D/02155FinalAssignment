9b6582ef216063455e92331367ac1904eb99eb1e
Thor Eric Dueholm <s194589@student.dtu.dk>
2021-11-09 13:48:07 +0100

    Can now pass every test in Task 1

--- src/IsaSim.java
+++ src/IsaSim.java
@@ -1,9 +1,10 @@
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 /**
  * RISC-V Instruction Set Simulator
@@ -20,58 +21,161 @@ public class IsaSim {
 
     // Here the first program hard coded as an array
     static byte[] buf;
-    static int[] progr;
+    static Integer[] progr;
+    static String input_path = "tests/task1/shift2.bin";
     static {
         try {
-            buf = Files.readAllBytes(Paths.get("tests\\task1\\addlarge.bin"));
+            buf = Files.readAllBytes(Paths.get(input_path));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
-    public static int[] convert(byte buf[]) {
-
-
-        int intArr[] = new int[buf.length / 4];
+    public static Integer[] convert(byte[] buf) {
+        ArrayList<Integer> list = new ArrayList<>();
         int offset = 0;
-        for(int i = 0; i < intArr.length; i++) {
+        for(int i = 0; i < buf.length / 4; i++) {
 
-            intArr[i] = (buf[3 + offset] & 0xFF)<< 24 | ((buf[2 + offset] & 0xFF) << 16) |
-                    ((buf[1 + offset] & 0xFF) << 8) | ((buf[0 + offset] & 0xFF) );
+            list.add((buf[3 + offset] & 0xFF)<< 24 | ((buf[2 + offset] & 0xFF) << 16) |
+                    ((buf[1 + offset] & 0xFF) << 8) | ((buf[0 + offset] & 0xFF)));
             offset += 4;
-            System.out.println(Integer.toHexString(intArr[i]));
+            System.out.println(Integer.toHexString(list.get(i)));
+            if (list.get(i) == 4) { //4 is at the end for every .bin file. We do not know why
+                list.remove(i);
+                break;
+            }
         }
-        return intArr;
+        Integer[] arr = new Integer[list.size()];
+        arr = list.toArray(arr);
+
+        return arr;
     }
 
     public static void main(String[] args) {
         progr = convert(buf);
         System.out.println("Hello RISC-V World!");
-        System.out.println(progr.length);
+
         pc = 0;
 
-        for (; ; ) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE << 1);
+
+        while(true) {
 
             int instr = progr[pc >> 2];
             int opcode = instr & 0x7f;
+            int funct3 = (instr >> 12) & 0x7;
+            int funct7 = (instr >> 25) & 0x7f;
             int rd = (instr >> 7) & 0x01f;
             int rs1 = (instr >> 15) & 0x01f;
             int rs2 = (instr >> 20) & 0x01f;
             int imm = (instr >> 20);
-            boolean flag = false;
-            switch (opcode) {
+            int immTypeU = (instr >> 12);
 
+            switch (opcode) {
                 case 0x13:
-                    reg[rd] = reg[rs1] + imm;
+                    switch (funct3) {
+                        case 0x0: //addi
+                            reg[rd] = reg[rs1] + imm;
+                            break;
+                        case 0x1: //slli
+                            reg[rd] = reg[rs1] << (imm & 0x1f);
+                            break;
+                        case 0x2: //slti
+                            if (rs1<imm) { reg[rd] = 1; }
+                            else reg[rd] = 0;
+                            break;
+                        case 0x3: //sltiu
+                            if ( (rs1 & 0xffffffffL) < (imm & 0xffffffffL)) { reg[rd] = 1; }
+                            else reg[rd] = 0;
+                            break;
+                        case 0x4: //xori
+                            reg[rd] = reg[rs1] ^ imm;
+                            break;
+                        case 0x5:
+                            switch (funct7) {
+                                case 0x0: //srli
+                                    reg[rd] = reg[rs1] >>> (imm & 0x1f);
+                                    break;
+                                case 0x20: //srai
+                                    reg[rd] = reg[rs1] >> (imm & 0x1f);
+                                    break;
+                                default:
+                                    System.out.println("opcode: " + Integer.toHexString(opcode) + " funct3: " + Integer.toHexString(funct3) + "funct7" + Integer.toHexString(funct7) + " not yet implemented");
+                                    break;
+                            }
+                            break;
+                        case 0x6: //ori
+                            reg[rd] = reg[rs1] | imm;
+                            break;
+                        case 0x7: //andi
+                            reg[rd] = reg[rs1] & imm;
+                            break;
+                        default:
+                            System.out.println("opcode: " + Integer.toHexString(opcode) + " funct3: " + Integer.toHexString(funct3) + " not yet implemented");
+                            break;
+                    }
                     break;
                 case 0x33:
-                    reg[rd] = reg[rs1] + reg[rs2];
+                    switch (funct3) {
+                        case 0x0:
+                            switch (funct7) {
+                                case 0x0: //add
+                                    reg[rd] = reg[rs1] + reg[rs2];
+                                    break;
+                                case 0x20: //sub
+                                    reg[rd] = reg[rs1] - reg[rs2];
+                                    break;
+                                default:
+                                    System.out.println("opcode: " + Integer.toHexString(opcode) + " funct3: " + Integer.toHexString(funct3) + "funct7" + Integer.toHexString(funct7) + " not yet implemented");
+                                    break;
+                            }
+                            break;
+                        case 0x1: //sll
+                            reg[rd] = reg[rs1] << (reg[rs2] & 0x1f);
+                            break;
+                        case 0x2: //slt
+                            if (rs1<rs2) { reg[rd] = 1; }
+                            else reg[rd] = 0;
+                            break;
+                        case 0x3: //sltu
+                            if ( (rs1 & 0xffffffffL) < (rs2 & 0xffffffffL)) { reg[rd] = 1; }
+                            else reg[rd] = 0;
+                            break;
+                        case 0x4: //xor
+                            reg[rd] = reg[rs1] ^ reg[rs2];
+                            break;
+                        case 0x5:
+                            switch (funct7) {
+                                case 0x0: //srl
+                                    reg[rd] = reg[rs1] >>> (reg[rs2] & 0x1f);
+                                    break;
+                                case 0x20: //sra
+                                    reg[rd] = reg[rs1] >> (reg[rs2] & 0x1f);
+                                    break;
+                                default:
+                                    System.out.println("opcode: " + Integer.toHexString(opcode) + " funct3: " + Integer.toHexString(funct3) + "funct7" + Integer.toHexString(funct7) + " not yet implemented");
+                                    break;
+                            }
+                            break;
+                        case 0x6: //or
+                            reg[rd] = reg[rs1] | reg[rs2];
+                            break;
+                        case 0x7: //and
+                            reg[rd] = reg[rs1] & reg[rs2];
+                            break;
+                        default:
+                            System.out.println("opcode: " + Integer.toHexString(opcode) + " funct3: " + Integer.toHexString(funct3) + " not yet implemented");
+                    }
                     break;
-                case 73:
-                    flag = true;
+                case 0x37: //lui
+                    reg[rd] = (immTypeU << 12) & 0xFFFFF000;
+                    break;
+                case 0x73: //ecall
+                    //TODO print everything
                     break;
                 default:
-                    System.out.println("Opcode " + opcode + " not yet implemented");
+                    System.out.println("Opcode " + Integer.toHexString(opcode)  + " not yet implemented");
                     break;
             }
 
@@ -82,7 +186,7 @@ public class IsaSim {
             }
             if ((pc >> 2) >= progr.length) {
                 break;
-            } if(flag) break;
+            }
             System.out.println();
         }
 
